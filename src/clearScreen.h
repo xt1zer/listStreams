0341ba51c032cdb1cc36751ecd3bb298dc2ef257
Daniel White <xt1zer@outlook.com>
2020-04-15 02:22:31 +0300

    Add safe console screen cleaner

---
+++ src/clearScreen.h
@@ -0,0 +1,55 @@
+#pragma once
+/* A universal way to clear console screen without using system calls */
+#ifdef _WIN32
+#include <windows.h>
+void clearScreen() {
+    HANDLE                     hStdOut;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    DWORD                      count;
+    DWORD                      cellCount;
+    COORD                      homeCoords = { 0, 0 };
+
+    hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+    /* Get the number of cells in the current buffer */
+    if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
+    cellCount = csbi.dwSize.X * csbi.dwSize.Y;
+
+    /* Fill the entire buffer with spaces */
+    if (!FillConsoleOutputCharacter(
+        hStdOut,
+        (TCHAR)' ',
+        cellCount,
+        homeCoords,
+        &count
+    )) return;
+
+/* Fill the entire buffer with the current colors and attributes */
+    if (!FillConsoleOutputAttribute(
+        hStdOut,
+        csbi.wAttributes,
+        cellCount,
+        homeCoords,
+        &count
+    )) return;
+
+/* Move the cursor home */
+    SetConsoleCursorPosition(hStdOut, homeCoords);
+}
+#endif
+
+#ifdef __unix__
+#include <unistd.h>
+#include <term.h>
+
+void clearScreen() {
+    if (!cur_term) {
+        int result;
+        setupterm(NULL, STDOUT_FILENO, &result);
+        if (result <= 0) return;
+    }
+
+    putp(tigetstr("clear"));
+}
+#endif
